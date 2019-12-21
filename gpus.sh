e6097be25eb70721d2d2b334b54a39380e084afe
Gary Mulder <gjmulder@gmail.com>
2019-12-21 13:56:28 +0200

    Added gpu ssh nvidia-smi monitor

---
+++ gpus.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ $# -eq 2 ]
+then
+	WAIT=$1
+	REP=$2
+else
+	WAIT=0
+	REP=1
+fi
+for X in `seq 1 $REP`
+do
+	echo "==============================================================================="
+	date
+	(nvidia-smi ;ssh yokojitsu nvidia-smi ; ssh asushimu nvidia-smi )| egrep "python3|Default" | sort -n
+	sleep $WAIT
+done
