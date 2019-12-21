52494906265ca24e8e49f529965834d4035ccf09
Gary Mulder <gjmulder@gmail.com>
2019-12-21 15:56:11 +0200

    Only output when there is a better result

--- chk_runs.sh
+++ chk_runs.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
+MD5_OLD=`cat /tmp/run_old | md5sum`
 . /var/tmp/dataset_version.sh
-/home/mulderg/bin/chk_mongo.sh "${DATASET}-${VERSION}" | grep -v MongoDB
+/home/mulderg/bin/chk_mongo.sh "${DATASET}-${VERSION}" 2>&1 | grep -v MongoDB > /tmp/run_new
+MD5_NEW=`cat /tmp/run_new | md5sum`
+if [ "$MD5_NEW" != "$MD5_OLD" ]
+then
+	echo cat /tmp/run_new
+fi
+mv /tmp/run_new /tmp/run_old
