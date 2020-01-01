91dd21ef08706b38b0a6bb8bbc43a2de44ec8849
Gary Mulder <gjmulder@gmail.com>
2020-01-01 18:02:27 +0200

    Updated MongoDB scripts

--- chk_mongo.sh
+++ chk_mongo.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-echo 'db.jobs.find({"result.status" : "ok"}).sort( { "result.loss": 1} ).limit(1).pretty()' | mongo --host heika $1 | awk 'BEGIN {skip=1} /result/ {skip=0} /misc/ {skip=1} {if (! skip) {print}}'
+if [ $# -eq 2 ]
+then
+	MODEL=$2
+	echo "db.jobs.find({\"result.status\" : \"ok\", \"result.cfg.model.type\" : \"${MODEL}\"}).sort( { \"result.loss\": 1} ).limit(1).pretty()"
+else
+	echo 'db.jobs.find({"result.status" : "ok"}).sort( { "result.loss": 1} ).limit(1).pretty()'
+fi | mongo --host heika $1 | awk 'BEGIN {skip=1} /result/ {skip=0} /misc/ {skip=1} {if (! skip) {print}}'
+
--- dump_jobs.sh
+++ dump_jobs.sh
@@ -3,17 +3,15 @@
 
 if [ $# -eq 0 ]
 then
-STATUS="ok"
+RESULT="ok"
 	DBS=`echo "show dbs" | mongo --host heika | awk '/GB$/ {print $1}' | egrep -v "^(admin|config|local)"`
 else
-	STATUS="$1"
-	shift
 	DBS=$*
 fi
 
 for DB in $DBS
 do
 	echo "DB name: $DB"
-	echo "db.jobs.find({\"result.status\" : \"$STATUS\"}).toArray()" | mongo --host heika $DB
+	echo "db.jobs.find({}).toArray()" | mongo --host heika $DB
 #| awk 'BEGIN {skip=1} /result/ {skip=0} /misc/ {skip=1} {if (! skip) {print}}'
 done
