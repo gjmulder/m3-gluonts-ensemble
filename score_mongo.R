44a7b40681a553f595cfbea51d8adeac5e68df4e
Gary Mulder <gjmulder@gmail.com>
2020-01-21 09:30:44 +0200

    Score 445 models

--- score_mongo.R
+++ score_mongo.R
@@ -3,6 +3,7 @@ library(jsonlite)
 library(matrixStats)
 # library(Metrics)
 library(lubridate)
+library(gtools)
 library(dplyr)
 library(ggplot2)
 
@@ -15,10 +16,10 @@ con <-
         verbose = TRUE)
 mydata <- con$find()
 print(mydata$result$cfg$model$type)
-fcasts <-
-  t(as.data.frame(Filter(
-    Negate(is.null), mydata$result$err_metrics$y_hats[[1]]
-  )))
+# fcasts <-
+#   t(as.data.frame(Filter(
+#     Negate(is.null), mydata$result$err_metrics$y_hats[[1]]
+  # )))
 rm(con)
 gc()
 
@@ -54,7 +55,7 @@ ensemble_fcasts <- function(col_idx) {
     # print(idx)
     ts_fcast <-
       as.matrix(as.data.frame(Filter(Negate(is.null), fcasts_df[[idx]])))
-    y_hat <- rowMeans(ts_fcast)
+    y_hat <- rowMedians(ts_fcast)
 
     test_row <- fromJSON(res[idx])
     in_sample <- head(test_row$target, length(test_row$target) - 18)
@@ -66,6 +67,7 @@ ensemble_fcasts <- function(col_idx) {
   return(list(sMAPE = mean(smapes), MASE = mean(mases)))
 }
 
+uniq_model_types <- sort(unique(model_types))
 results <-
   data.frame(
     model.type = character(),
@@ -73,95 +75,129 @@ results <-
     MASE = double(),
     sMAPE = double()
   )
-print(results)
-
-# Indvidual models
-uniq_model_types <- sort(unique(model_types))
-for (model_type in uniq_model_types) {
-  col_idx <- which(model_type == model_types)
-  errs <- ensemble_fcasts(col_idx)
-  result <-
-    data.frame(
-      model.type = model_type,
-      number.models = length(col_idx),
-      sMAPE = errs[['sMAPE']],
-      MASE = errs[['MASE']]
-    )
-  print(result)
-  results <- rbind(results, result)
+for (idx1 in 1:length(uniq_model_types)) {
+  comb <- combinations(length(uniq_model_types), idx1, uniq_model_types)
+  for (idx2 in 1:nrow(comb)) {
+    print(comb[idx2,])
+    col_idx <- c()
+    for (model_type in comb[idx2,]) {
+      col_idx <- c(col_idx, which(model_type == model_types))
+      print(length(col_idx))
+    }
+    errs <- ensemble_fcasts(col_idx)
+    result <-
+      data.frame(
+        model.type = paste0(comb[idx2,], collapse = "+"),
+        number.models = length(col_idx),
+        sMAPE = errs[['sMAPE']],
+        MASE = errs[['MASE']]
+      )
+    # print(result)
+    results <- rbind(results, result)
+  }
 }
 
-# Ensembled, leave one model type out
-for (model_type in uniq_model_types) {
-  col_idx <- which(!is.na(model_types) & (model_type != model_types))
-  model_type <-
-    paste0(sort(unique(model_types[col_idx]), na.last = NA), collapse = "+")
-  errs <- ensemble_fcasts(col_idx)
-  result <-
-    data.frame(
-      model.type = model_type,
-      number.models = length(col_idx),
-      sMAPE = errs[['sMAPE']],
-      MASE = errs[['MASE']]
-    )
-  print(result)
-  results <- rbind(results, result)
-}
+# # Ensembled, leave one model type out
+# for (model_type in uniq_model_types) {
+#   col_idx <- which(!is.na(model_types) & (model_type != model_types))
+#   model_type <-
+#     paste0(sort(unique(model_types[col_idx]), na.last = NA), collapse = "+")
+#   errs <- ensemble_fcasts(col_idx)
+#   result <-
+#     data.frame(
+#       model.type = model_type,
+#       number.models = length(col_idx),
+#       sMAPE = errs[['sMAPE']],
+#       MASE = errs[['MASE']]
+#     )
+#   print(result)
+#   results <- rbind(results, result)
+# }
+#
+# # Ensembled DeepAREstimator + TransformerEstimator
+# col_idx <-
+#   which(model_types %in% c("DeepAREstimator", "TransformerEstimator"))
+# model_type <-
+#   paste0(c("DeepAREstimator", "TransformerEstimator"), collapse = "+")
+# result <-
+#   data.frame(
+#     model.type = model_type,
+#     number.models = length(col_idx),
+#     sMAPE = errs[['sMAPE']],
+#     MASE = errs[['MASE']]
+#   )
+# print(result)
+# results <- rbind(results, result)
+#
+# # Ensembled DeepAREstimator + TransformerEstimator
+# col_idx <-
+#   which(model_types %in% c("DeepAREstimator", "TransformerEstimator"))
+# model_type <-
+#   paste0(c("DeepAREstimator", "TransformerEstimator"), collapse = "+")
+# result <-
+#   data.frame(
+#     model.type = model_type,
+#     number.models = length(col_idx),
+#     sMAPE = errs[['sMAPE']],
+#     MASE = errs[['MASE']]
+#   )
+# print(result)
+# results <- rbind(results, result)
+#
+# # Ensembled DeepAREstimator + TransformerEstimator
+# col_idx <-
+#   which(model_types %in% c("DeepAREstimator", "TransformerEstimator"))
+# model_type <-
+#   paste0(c("DeepAREstimator", "TransformerEstimator"), collapse = "+")
+# result <-
+#   data.frame(
+#     model.type = model_type,
+#     number.models = length(col_idx),
+#     sMAPE = errs[['sMAPE']],
+#     MASE = errs[['MASE']]
+#   )
+# print(result)
+# results <- rbind(results, result)
+#
+# # Ensembled everything
+# col_idx <- which(!is.na(model_types))
+# model_type <- paste0(uniq_model_types, collapse = "+")
+# result <-
+#   data.frame(
+#     model.type = model_type,
+#     number.models = length(col_idx),
+#     sMAPE = errs[['sMAPE']],
+#     MASE = errs[['MASE']]
+#   )
+# print(result)
+# results <- rbind(results, result)
 
-# Ensembled DeepAREstimator + TransformerEstimator
-col_idx <-
-  which(model_types %in% c("DeepAREstimator", "TransformerEstimator"))
-model_type <-
-  paste0(c("DeepAREstimator", "TransformerEstimator"), collapse = "+")
-result <-
-  data.frame(
-    model.type = model_type,
-    number.models = length(col_idx),
-    sMAPE = errs[['sMAPE']],
-    MASE = errs[['MASE']]
-  )
-print(result)
-results <- rbind(results, result)
+# # Sampling
+# col_idx_all <- which(!is.na(model_types))
+# for (num_samples in c(10:length(col_idx_all))) {
+#   col_idx <- sample(col_idx_all, size = num_samples)
+#   errs <- ensemble_fcasts(col_idx)
+#   result <-
+#     data.frame(
+#       number.models = length(col_idx),
+#       sMAPE = errs[['sMAPE']],
+#       MASE = errs[['MASE']]
+#     )
+#   print(result)
+#   results <- rbind(results, result)
+# }
+# print(results)
 
-# Ensembled everything
-col_idx <- which(!is.na(model_types))
-model_type <- paste0(uniq_model_types, collapse = "+")
-result <-
-  data.frame(
-    model.type = model_type,
-    number.models = length(col_idx),
-    sMAPE = errs[['sMAPE']],
-    MASE = errs[['MASE']]
-  )
-print(result)
-results <- rbind(results, result)
-
-# Sampling
-col_idx_all <- which(!is.na(model_types))
-for (num_samples in c(10:length(col_idx_all))) {
-  col_idx <- sample(col_idx_all, size = num_samples)
-  errs <- ensemble_fcasts(col_idx)
-  result <-
-    data.frame(
-      number.models = length(col_idx),
-      sMAPE = errs[['sMAPE']],
-      MASE = errs[['MASE']],
-    )
-  print(result)
-  results <- rbind(results, result)
-}
-print(results)
-
-gg <-
-  ggplot(results, aes(x = number.models, y = sMAPE * 100)) +
-  geom_point(size = 0.1) +
-  geom_smooth(size = 0.5, se = FALSE) +
-  labs(
-    title = "18 month forecast sMAPE versus number of ensembled models",
-    x = "Number of models",
-    y = "18 month sMAPE"
-  )
-print(gg)
+# gg <-
+#   ggplot(results, aes(x = number.models, y = MASE)) +
+#   geom_point(size = 0.1) +
+#   geom_smooth(size = 0.5, se = FALSE) +
+#   labs(
+#     title = "18 month forecast MASE versus number of ensembled models",
+#     x = "Number of models",
+#     y = "18 month MASE"
+#   )
+# print(gg)
 
 # tibble(model.type = model_types, book.time = ymd_hms(mydata$book_time, tz = "EET"), refresh.time = ymd_hms(mydata$refresh_time, tz = "EET")) %>%
 #   mutate(duration = refresh.time - book.time) ->
