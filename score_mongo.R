0b0fb1c3114da13f26d19aac9ce7f5173b7c1315
Gary Mulder <gjmulder@gmail.com>
2020-01-27 06:32:48 +0200

    Broken 1-step for 18+1 horiz fcasts

--- score_mongo.R
+++ score_mongo.R
@@ -1,30 +1,12 @@
 library(mongolite)
 library(jsonlite)
 library(matrixStats)
-# library(Metrics)
 library(lubridate)
 library(gtools)
 library(dplyr)
 library(ggplot2)
 
-options(width = 150)
-
-con <-
-  mongo("jobs",
-        "m3_monthly-final_rnd_1-step",
-        url = "mongodb://heika:27017",
-        verbose = TRUE)
-mydata <- con$find()
-print(mydata$result$cfg$model$type)
-# fcasts <-
-#   t(as.data.frame(Filter(
-#     Negate(is.null), mydata$result$err_metrics$y_hats[[1]]
-# )))
-rm(con)
-gc()
-
-model_types <- mydata$result$cfg$model$type
-res <- readLines("./m3_monthly_all/test/data.json")
+set.seed(42)
 
 smape_cal <- function(outsample, forecasts) {
   #Used to estimate sMAPE
@@ -36,9 +18,13 @@ smape_cal <- function(outsample, forecasts) {
   return(smape)
 }
 
-mase_cal <- function(insample, outsample, forecasts) {
-  #Used to estimate MASE
-  frq <- 12
+mase_cal <- function(insample, outsample, forecasts, frq) {
+  stopifnot(!is.na(insample),
+            !is.na(outsample),
+            !is.na(forecasts),
+            !is.na(frq))
+
+  # Used to estimate MASE
   forecastsNaiveSD <- rep(NA, frq)
   for (j in (frq + 1):length(insample)) {
     forecastsNaiveSD <- c(forecastsNaiveSD, insample[j - frq])
@@ -49,11 +35,14 @@ mase_cal <- function(insample, outsample, forecasts) {
     as.numeric(outsample)
   forecasts <- as.numeric(forecasts)
   mase <- (abs(outsample - forecasts)) / masep
+
+  stopifnot(!is.na(mase),!is.nan(mase))
   return(mase)
 }
 
 ensemble_fcasts <- function(col_idx) {
-  fcasts_df <- mydata$result$err_metrics$y_hats[col_idx, ]
+  # print(col_idx)
+  fcasts_df <- mongo_data$result$err_metrics$y_hats[col_idx, ]
   smapes <- c()
   mases <- c()
 
@@ -61,61 +50,243 @@ ensemble_fcasts <- function(col_idx) {
     # print(idx)
     ts_fcast <-
       as.matrix(as.data.frame(Filter(Negate(is.null), fcasts_df[[idx]])))
-    y_hat <- rowMedians(ts_fcast)
+    if (ncol(ts_fcast) == 0) {
+      break
+    }
+    y_hat <- rowMedians(ts_fcast, na.rm = TRUE)
 
     test_row <- fromJSON(res[idx])
-    in_sample <- head(test_row$target, length(test_row$target) - 18)
-    y_test <- head(tail(test_row$target, 18), 1)
+    in_sample <-
+      head(test_row$target, length(test_row$target) - length(y_hat))
+    y_test <- tail(test_row$target, length(y_hat))
 
-    smapes <- c(smapes, smape_cal(y_test, y_hat))
-    mases <- c(mases, mase_cal(in_sample, y_test, y_hat))
-
+    smapes <- c(smapes, mean(smape_cal(y_test, y_hat)))
+    mases <- c(mases, mean(mase_cal(in_sample, y_test, y_hat, frq)))
   }
-  return(
-    list(
-      sMAPE = mean(smapes),
-      MASE = mean(mases)
-    )
-  )
+  return(list(sMAPE = mean(smapes),
+              MASE = mean(mases)))
 }
 
-uniq_model_types <- sort(unique(model_types))
+build_ensemble <- function(model_set) {
+  uniq_model_types <- sort(unique(model_set$type))
+  # print(uniq_model_types)
 
-results <-
+  results_comb <-
+    data.frame(
+      model.type = character(),
+      number.models = integer(),
+      MASE = double(),
+      sMAPE = double()
+    )
+  for (idx1 in 1:length(uniq_model_types)) {
+    comb <-
+      combinations(length(uniq_model_types), idx1, uniq_model_types)
+    for (idx2 in 1:nrow(comb)) {
+      col_idx <- c()
+      for (model_type in comb[idx2, ]) {
+        model_col_idx <- which(model_type == model_set$type)
+        # model_col_idx <-
+        #   sample(model_col_idx, size = min(timings$`n()`), replace = FALSE)
+        col_idx <- c(col_idx, model_col_idx)
+      }
+      errs <- ensemble_fcasts(col_idx)
+      result <-
+        data.frame(
+          model.type = paste0(comb[idx2, ], collapse = "+"),
+          model.type.count = length(comb[idx2, ]),
+          total.num.models = length(col_idx),
+          MASE    = errs[['MASE']],
+          sMAPE   = errs[['sMAPE']]
+        )
+      results_comb <- rbind(results_comb, result)
+    }
+  }
+  return(results_comb)
+}
+
+####################################################################################
+# Get forecasts from MongoDB and test data from .json
+data_set <- "monthly_m3_1045"
+
+if (grepl("monthly", data_set)) {
+  frq = 12
+} else if (grepl("quarterly", data_set)) {
+  frq = 4
+} else if (grepl("yearly", data_set)) {
+  frq = 1
+} else if (grepl("other", data_set)) {
+  frq = 1
+} else {
+  stop("Unrecognised data set. Can't determine frequency")
+}
+print(paste0("Determined frequency from data set: ", frq))
+
+con <-
+  mongo("jobs",
+        "m3_monthly-final_rnd_1-step",
+        url = "mongodb://heika:27017",
+        verbose = TRUE)
+mongo_data1 <- con$find()
+rm(con)
+con <-
+  mongo("jobs",
+        "m3_monthly-final_rnd_6-18_mon",
+        url = "mongodb://heika:27017",
+        verbose = TRUE)
+mongo_data2 <- con$find()
+rm(con)
+gc()
+mongo_data <-
+  bind_rows(mongo_data1, mongo_data2)
+
+model_type_loss <-
+    tibble(
+      row.id = c(1:length(mongo_data$result$cfg$model$type)),
+      type = mongo_data$result$cfg$model$type,
+      loss = mongo_data$result$loss
+    )
+
+res <-
+  readLines("/home/mulderg/Work/plos1-m3/m3_monthly_all_1045/test/data.json")
+
+# ####################################################################################
+# # Timings
+#
+# tibble(
+#   model.type = model_type_loss$type,
+#   book.time = ymd_hms(mongo_data$book_time[model_type_loss$row.id], tz = "EET"),
+#   refresh.time = ymd_hms(mongo_data$refresh_time[model_type_loss$row.id], tz = "EET")
+# ) %>%
+#   mutate(duration = refresh.time - book.time) %>%
+#   na.omit %>%
+#   group_by(model.type) %>%
+#   summarise(n(), mean(duration), sd(duration)) ->
+#   timings
+# print(timings)
+
+####################################################################################
+# Model combinations
+
+# for (n in c(70, 80, 90, 100)) {
+# model_type_loss %>%
+# filter(type == "DeepAREstimator") ->
+# na.omit %>%
+# group_by(type) %>%
+# sample_n(n, replace = FALSE) ->
+# top_n(-n, loss) ->
+# top_frac(-n/100, loss) ->
+# best_models
+# print(best_models)
+
+build_ensemble(model_type_loss) %>%
+  na.omit %>%
+  mutate(model.type = gsub("Estimator", "", model.type)) %>%
+  mutate(model.count.fact = factor(paste0(
+    total.num.models, " [", model.type.count, "]"
+  ))) %>%
+  arrange(desc(sMAPE)) ->
+  results_comb
+# results_comb$n.star.models <- rep(n, nrow(results_comb))
+print(tail(results_comb[, c("model.type", "total.num.models", "MASE", "sMAPE")], 100))
+#}
+
+gg_comb <-
+  results_comb %>%
+  mutate(model.count.fact = reorder(model.count.fact, total.num.models)) %>%
+  ggplot(aes(x = model.count.fact, y = sMAPE)) +
+  geom_violin() +
+  # scale_y_log10() +
+  stat_summary(
+    fun.y = median,
+    geom = "point",
+    size = 1,
+    color = "red"
+  ) +
+  ylim(NA, 25.0) +
+  labs(title = "Forecast error versus number of ensembled combinations of models",
+       x = "Ensemble Size [Number of model types per ensemble]")
+
+####################################################################################
+# Sampling
+
+# col_idx_all <-
+#   which(!is.na(model_type_loss$type) & model_type_loss$type == "TransformerEstimator")
+col_idx_all <-
+  which(!is.na(model_type_loss$type))
+
+results_size <-
   data.frame(
     model.type = character(),
     number.models = integer(),
     MASE = double(),
     sMAPE = double()
-   )
-for (idx1 in 1:length(uniq_model_types)) {
-  comb <-
-    combinations(length(uniq_model_types), idx1, uniq_model_types)
-  for (idx2 in 1:nrow(comb)) {
-    print(comb[idx2, ])
-    col_idx <- c()
-    for (model_type in comb[idx2, ]) {
-      model_col_idx <- which(model_type == model_types)
-      col_idx <- c(col_idx, model_col_idx)
-      print(length(col_idx))
-    }
-    errs <- ensemble_fcasts(col_idx)
-     result <-
-      data.frame(
-        model.type = paste0(comb[idx2, ], collapse = "+"),
-        MASE    = errs[['MASE']],
-        sMAPE   = errs[['sMAPE']]
-      )
-    print(result)
-    results <- rbind(results, result)
-  }
+  )
+for (num_samples in c(25:length(col_idx_all))) {
+  col_idx <- sample(col_idx_all, size = num_samples, replace = TRUE)
+  errs <- ensemble_fcasts(col_idx)
+  result <-
+    data.frame(
+      number.models = length(col_idx),
+      sMAPE   = errs[['sMAPE']],
+      MASE    = errs[['MASE']]
+    )
+
+  results_size <- rbind(results_size, result)
 }
-write.csv(results, file = "ensemble_results.csv", row.names = FALSE, quote = FALSE)
-print(results)
+
+gg_size <-
+  ggplot(results_size, aes(x = number.models, y = sMAPE)) +
+  geom_point(size = 0.1) +
+  geom_smooth(size = 0.5, se = FALSE) +
+  labs(title = "Forecast error versus number of ensembled models",
+       x = "Number of models")
+
+if (!interactive()) {
+  options(width = 1000)
+
+  # write.csv(
+  #   timings,
+  #   file = paste0(data_set, "-", version, "-timings.csv"),
+  #   row.names = FALSE,
+  #   quote = FALSE
+  # )
+
+  write.csv(
+    results_comb,
+    file = paste0(data_set, "-", version, "-ensemble_combination_results.csv"),
+    row.names = FALSE,
+    quote = FALSE
+  )
+
+  ggsave(
+    paste0(data_set, "-", version, "-ensemble_combination_results.png"),
+    gg_comb,
+    width = 8,
+    height = 6
+  )
+
+  write.csv(
+    results_size,
+    file = paste0(data_set, "-", version, "-ensemble_err_vs_size.csv"),
+    row.names = FALSE,
+    quote = FALSE
+  )
+
+  ggsave(
+    paste0(data_set, "-", version, "-ensemble_err_vs_size.png"),
+    gg_size,
+    width = 8,
+    height = 6
+  )
+
+} else {
+  print(gg_comb)
+  print(gg_size)
+}
 
 # for (model_type in uniq_model_types) {
 #   col_idx <- which(model_types == model_type)
-#   MASEs <- tibble(MASE = mydata$result$err_metrics$validate$MASE[col_idx])
+#   MASEs <- tibble(MASE = mongo_data$result$err_metrics$validate$MASE[col_idx])
 #   gg <-
 #     ggplot(MASEs, aes(x = MASE)) +
 #     geom_histogram(bins = 20) +
@@ -124,11 +295,3 @@ print(results)
 #          y = "Count")
 #   print(gg)
 # }
-
-tibble(model.type = model_types, book.time = ymd_hms(mydata$book_time, tz = "EET"), refresh.time = ymd_hms(mydata$refresh_time, tz = "EET")) %>%
-  mutate(duration = refresh.time - book.time) ->
-  timings
-#
-timings %>%
-  group_by(model.type) %>%
-  summarise(mean(duration))
